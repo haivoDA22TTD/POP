90d17856e48d0437fd4df25c06e12b14774f5e36
haivoDA22TTD <vochihai7@gmail.com>
2025-10-23 08:27:33 +0700

    Update array

---
+++ array/array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+	
+	int main(){
+		
+		int n[10];
+		int m[] = {10, 20, 30};
+		int z[5] = {1, 2, 3, 4, 5};
+
+		printf("Array 1 %p\n", n);
+		printf("Array 2 %p\n", m);
+		printf("Array 3 %p\n", z);
+
+		return 0;
+	}
